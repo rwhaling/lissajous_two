43eacd5875da9b466734bb428262485b03b28d6e
Richard Whaling <rwhaling@gmail.com>
2025-05-12 10:52:20 -0400

    maybe this works?

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "./", // Set base path for relative asset loading
+  // You can add other Vite configurations here later if needed
+}) 
